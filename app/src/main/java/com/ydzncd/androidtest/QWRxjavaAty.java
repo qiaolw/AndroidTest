13347ff6710cccc9a46e00b7a687fc9dac627f58
rd60 <rd60@ce-link.com>
2018-06-24 23:43:58 +0800

    rxjava oper test『』

--- app/src/main/java/com/ydzncd/androidtest/QWRxjavaAty.java
+++ app/src/main/java/com/ydzncd/androidtest/QWRxjavaAty.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.reactivestreams.Publisher;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Flowable;
@@ -12,9 +17,12 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.Timed;
 
 
 public class QWRxjavaAty extends Activity {
@@ -103,6 +111,30 @@ public class QWRxjavaAty extends Activity {
     }
     @OnClick(R.id.bt_rxjava_flatMap)
     public void onRxjavaFlatMapTestClick(){
+        ArrayList<String[]> list=new ArrayList<>();
+        String[] words1={"Hello,","I am","China!"};
+        String[] words2={"Hello,","I am","Beijing!"};
+        list.add(words1);
+        list.add(words2);
+
+        Flowable.fromIterable(list)
+                .flatMap(new Function<String[], Publisher<String>>() {
+                    @Override
+                    public Publisher<String> apply(String[] strings) throws Exception {
+                        return Flowable.fromArray(strings);
+                    }
+                })
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e("consumer", s.toString());
+                    }
+                });
+
+    }
+
+    @OnClick(R.id.bt_rxjava_concatMap)
+    public void onRxjavaConcatMapClick(){
 
     }
     @OnClick(R.id.bt_rxjava_Flowable)
@@ -132,7 +164,192 @@ public class QWRxjavaAty extends Activity {
                 Log.e("qob", "accept " + integer);
             }
         });
-}
+    }
+
+    @OnClick(R.id.bt_rxjava_zip)
+    public void onRxjavaZipClick(){
+        Observable.zip(Observable.just(1, 2, 3,7), Observable.just(4, 5, 6), new BiFunction<Integer, Integer, Integer>() {
+            @Override
+            public Integer apply(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.e("qob", "zip " + integer);
+            }
+        });
+
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onComplete();
+                e.onNext(2);
+                Log.e("qob", "zip observable1 emitter");
+
+            }
+        });
+
+        Observable<Integer> observable2 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(4);
+                e.onNext(5);
+                Log.e("qob", "zip observable2 emitter");
+
+            }
+        });
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, Integer, Integer>() {
+
+            @Override
+            public Integer apply(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        }).subscribe(new Consumer<Object>() {
+
+            @Override
+            public void accept(Object o) throws Exception {
+                Log.e("qob", "zip Consumer " + o);
+            }
+        });
+    }
+
+    @OnClick(R.id.bt_rxjava_concat)
+    public void onRxjavaConcatClick(){
+        Observable.concat(Observable.just(1,2,3), Observable.just(4,5,6))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e("qob", "Concat " + integer);
+                    }
+                });
+
+        Observable.concat(Observable.just(1, 2, 3), Observable.just(5, 6 ,7))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e("qob", "concat 1 " + integer);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.e("qob", "ThrowAble " + throwable);
+                    }
+                });
+
+        Observable observable1 = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(ObservableEmitter e) throws Exception {
+                e.onNext(1);
+                Log.e("qob", "observable1 1");
+                e.onNext(2);
+                Log.e("qob", "observable1 2");
+
+         //       e.onError(new Throwable("aaaaa"));
+                e.onComplete();  // observable1 一定要调用onComlete()后才会 遍历observable2
+            }
+        });
+
+        Observable observable2 = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(ObservableEmitter e) throws Exception {
+                e.onNext(4);
+                Log.e("qob", "observable2 4");
+                e.onNext(5);
+                Log.e("qob", "observable2 5");
+            }
+        });
+
+        Observable.concat(observable1, observable2).timeInterval()
+                .subscribe(new Consumer() {
+                    @Override
+                    public void accept(Object o) throws Exception {
+                        Log.e("qob", "consumer " + o);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.e("qob", "Throwable " + throwable);
+                    }
+                });
+
+
+    }
+
+    @OnClick(R.id.bt_rxjava_time)
+    public void onRxjavaTimeClick(){
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.e("qob", "observable emitter");
+                e.onNext(1000);
+            }
+        }).delay(10, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.e("qob", "delay " + integer);
+            }
+        });
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.e("qob", "observable emitter");
+                e.onNext(1000);
+            }
+        }).timeInterval().subscribe(new Consumer<Timed<Integer>>() {
+            @Override
+            public void accept(Timed<Integer> integerTimed) throws Exception {
+                Log.v(TAG, "accept:"+integerTimed);
+            }
+        });
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                Thread.sleep(3000);
+                e.onNext(2);
+            }
+        }).timeout(2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e("qob", "onSubscribe");
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e("qob", "onNext " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("qob", "onError " + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("qob", "onComplete");
+                    }
+                });
+
+        Observable.interval(200, TimeUnit.MILLISECONDS).subscribe(new Consumer<Long>() {
+            @Override
+            public void accept(Long aLong) throws Exception {
+                Log.e("qob", "interval accept " + aLong);
+            }
+        });
+    }
+
+
 
     private void readme(){
         //http://gank.io/post/560e15be2dca930e00da1083#toc_2
