a0347b5c7d12bf0673b804def8bebb9904c336f4
qiaoliwei-pc <qiaolw0709@foxmail.com>
2018-06-25 09:53:50 +0800

    delete no use code

--- app/src/main/java/com/ydzncd/androidtest/QWLocationAty.java
+++ app/src/main/java/com/ydzncd/androidtest/QWLocationAty.java
@@ -8,8 +8,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
