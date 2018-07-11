211167903fea26ccbf6d2b30334b75be638dd084
qiaolw <qiaolw0709@foxmail.com>
2018-07-11 08:27:14 +0800

    runtime reflect paser annotation

--- app/src/main/java/com/ydzncd/androidtest/GWAnnotationAty.java
+++ app/src/main/java/com/ydzncd/androidtest/GWAnnotationAty.java
@@ -2,8 +2,13 @@ package com.ydzncd.androidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.ydzncd.androidtest.Annotation.MyAnnotation1;
 import com.ydzncd.androidtest.Annotation.MyEntity1;
+
+import java.lang.reflect.Method;
+
 /**
  *自定义gradle插件，处理Annotaion https://blog.csdn.net/a568478312/article/details/8022508
  */
@@ -15,7 +20,18 @@ public class GWAnnotationAty extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gwannotation_aty);
 
-        MyEntity1 tEntity1 = new MyEntity1();
-        tEntity1.sayHello();
+        trackMyEntity(MyEntity1.class);
+    }
+
+    private void trackMyEntity(Class<?> cl){
+        for (Method m : cl.getDeclaredMethods()){
+            MyAnnotation1 mA = m.getAnnotation(MyAnnotation1.class);
+            if (mA != null){
+                Log.e("qob", "Found MyAnnotation1 tag: " + mA.tag() + " des " + mA.desc());
+            }
+            else {
+                Log.e("qob", "Annotation is NUll method: " + m);
+            }
+        }
     }
 }
