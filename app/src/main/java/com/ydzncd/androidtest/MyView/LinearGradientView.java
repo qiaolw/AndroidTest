086dd98d1d3dda396afbcbe63af17f1decfb1f96
qiaoliwei-pc <qiaolw0709@foxmail.com>
2018-07-19 21:17:44 +0800

    test LinearGradientView

--- app/src/main/java/com/ydzncd/androidtest/MyView/LinearGradientView.java
+++ app/src/main/java/com/ydzncd/androidtest/MyView/LinearGradientView.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class LinearGradientView extends View {
@@ -22,7 +23,7 @@ public class LinearGradientView extends View {
     public LinearGradientView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        linearGradient = new LinearGradient(0, 0, 100, 100, new int[] {Color.RED, Color.WHITE, Color.BLUE}, new float[]{0, 300,500},
+        linearGradient = new LinearGradient(0, 0, 0, 300, new int[] {Color.RED,Color.GREEN, Color.BLUE}, new float[]{0.3f, 0.5f, 0.7f},
                 Shader.TileMode.CLAMP);
         paint = new Paint();
     }
@@ -31,14 +32,21 @@ public class LinearGradientView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        Log.e("qob", "width: " + canvas.getWidth() + " height " + canvas.getHeight());
+
         //设置渲染器
+        paint.setAntiAlias(true);
         paint.setShader(linearGradient);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5f);
+        paint.setColor(getResources().getColor(android.R.color.holo_blue_light));//填充颜色
+
         //绘制圆环
-//        Path tLinePath = new Path();
-//        tLinePath.moveTo(50, 50);
-//        tLinePath.lineTo(400, 150);
-//        tLinePath.close();
-//        canvas.drawPath(tLinePath, paint);
-        canvas.drawRect(0, 0, 500, 200, paint);
+        Path tLinePath = new Path();
+        tLinePath.moveTo(50, 0);
+        tLinePath.cubicTo(50,0,150,300,500, 200);
+        tLinePath.cubicTo(500,200,600,0,700, 200);
+        canvas.drawPath(tLinePath, paint);
+//        canvas.drawRect(0, 0, 500, 200, paint);
     }
 }
