4ca48070fef53079d801dd2af9e780709818bc34
qiaoliwei-pc <qiaolw0709@foxmail.com>
2018-08-27 18:02:30 +0800

    test socket send

---
+++ app/src/main/java/com/ydzncd/androidtest/IO/TcpConnection.java
@@ -0,0 +1,177 @@
+package com.ydzncd.androidtest.IO;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+
+public class TcpConnection {
+
+    public interface TCPCallback {
+
+        void onDisconnected(TcpConnection tcpConn);
+
+        void onConnected(TcpConnection tcpConn);
+
+        void onReceiveData(TcpConnection tcpConn, byte[] data);
+
+        void onWriteFailed(TcpConnection tcpConn);
+
+        void onWriteSuccess(TcpConnection tcpConn);
+    }
+
+    private WorkThread mWorkThread;
+    private TCPCallback mCallback;
+
+    public TcpConnection(TCPCallback callback) {
+        mCallback = callback;
+    }
+
+    public boolean write(byte[] out) {
+        WorkThread t;
+        // 创建临时的TransThread, 使写数据的操作, 不在同步块中
+        synchronized (this) {
+            if (mWorkThread == null || mWorkThread.mOutStream == null) {
+                mCallback.onWriteFailed(TcpConnection.this);
+                return false;
+            }
+            t = mWorkThread;
+        }
+        return t.write(out);
+    }
+
+    public synchronized boolean connect() {
+
+        String tHost = "www.honggps.com";
+        int tPort = 8882;
+
+        Log.e("qob", "开始连接 Host：" + tHost + " Port " + tPort);
+        if (false) {  //判断网络是否OK
+            disconnectAndCallback();
+            return false;
+        }
+
+        disconnect();
+        mWorkThread = new WorkThread();
+        mWorkThread.start();
+
+        return true;
+    }
+
+    public synchronized void disconnect() {
+        if (mWorkThread != null) {
+            mWorkThread.cancel();
+            mWorkThread.mOutStream = null;
+            mWorkThread = null;
+        }
+    }
+
+    private void disconnectAndCallback() {
+        disconnect();
+        mCallback.onDisconnected(this);
+    }
+
+    private class WorkThread extends Thread {
+
+        private boolean needdisconnect = false;
+
+        private boolean isConnecting = false;
+
+        private Socket mSocket;
+        private InputStream mInStream;
+        private OutputStream mOutStream;
+        public WorkThread() {
+            super(WorkThread.class.getSimpleName());
+        }
+        @Override
+        public void run() {
+            // 连接
+            try {
+                if (needdisconnect) {
+                    this.cancel();
+                    return;
+                }
+                isConnecting = true;
+                mSocket = new Socket("www.honggps.com", 8882);
+                //           mSocket.connect();
+                Log.e("qob", "连接完成");
+                isConnecting = false;
+                if (needdisconnect) {
+                    Log.e("qob", "连接成功后，需要断开");
+                    this.cancel();
+                    return;
+                }
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+                isConnecting = false;
+                disconnectAndCallback();
+                Log.e("qob", "Socket连接异常:" + e.toString());
+                return;
+            }
+            finally {
+
+            }
+            // 获取读写流
+            try {
+                mInStream = mSocket.getInputStream();
+                mOutStream = mSocket.getOutputStream();
+                Log.e("qob", "流获取成功");
+            } catch (IOException e) {
+                Log.e("qob", "流获取失败");
+                e.printStackTrace();
+                disconnectAndCallback();
+                return;
+            }
+            // 连接成功
+            mCallback.onConnected(TcpConnection.this);
+
+            // 循环读取数据
+            byte[] buffer = new byte[2048];
+            while (true) try {
+                int length = mInStream.read(buffer);
+                if (length < 0){
+                    break;
+                }
+                byte[] bytes = new byte[length];
+                System.arraycopy(buffer, 0, bytes, 0, length);
+                mCallback.onReceiveData(TcpConnection.this, bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+                disconnectAndCallback();
+                return;
+            }
+        }
+
+        public boolean write(byte[] bytes) {
+            try {
+                mOutStream.write(bytes);
+                mCallback.onWriteSuccess(TcpConnection.this);
+                Log.d("qob", "onWriteSuccess: " + Arrays.toString(bytes));
+            } catch (IOException e) {
+                e.printStackTrace();
+                mCallback.onWriteFailed(TcpConnection.this);
+                return false;
+            }
+            return true;
+        }
+
+        public void cancel() {
+            needdisconnect = true;
+            try {
+                if (mSocket != null) {
+                    mSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            this.interrupt();// 唤醒sleep
+        }
+    }
+}
---
+++ app/src/main/java/com/ydzncd/androidtest/QWIOAty.java
@@ -0,0 +1,73 @@
+package com.ydzncd.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.ydzncd.androidtest.IO.TcpConnection;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class QWIOAty extends Activity implements TcpConnection.TCPCallback {
+    private TcpConnection mClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qwioaty);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_socket)
+    public void onSocketClicked(){
+        mClient = new TcpConnection(this);
+        mClient.connect();
+    }
+
+    @Override
+    public void onDisconnected(TcpConnection tcpConn) {
+        Log.e("qob", "onDisconnected");
+    }
+
+    @Override
+    public void onConnected(TcpConnection tcpConn) {
+        Log.e("qob", "onConnected");
+        Gson gson = new Gson();
+
+        Map map = new HashMap();
+        map.put("cmd", "02");
+        map.put("sid", "xxx");
+        Map tData = new HashMap();
+        tData.put("UserName", "lrs");
+        tData.put("Pwd", "123456");
+        map.put("data", tData);
+        String tGsonStr = gson.toJson(map) + "\r\n";
+        Log.e("qob", "tGsonStr " + tGsonStr);
+        try {
+            mClient.write(tGsonStr.getBytes("utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onReceiveData(TcpConnection tcpConn, byte[] data) {
+        Log.e("qob", "onReceiveData");
+    }
+
+    @Override
+    public void onWriteFailed(TcpConnection tcpConn) {
+        Log.e("qob", "onWriteFailed");
+    }
+
+    @Override
+    public void onWriteSuccess(TcpConnection tcpConn) {
+        Log.e("qob", "onWriteSuccess");
+    }
+}
